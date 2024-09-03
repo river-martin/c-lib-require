183ed7715e7e6af0dc219dc918a43334bbfea284
river-martin <rivermartin128@gmail.com>
2024-09-03 15:36:57 +0200

    Add files

---
+++ tests/test_try_catch.c
@@ -0,0 +1,21 @@
+#include <require.h>
+#include <assert.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void something_that_will_fail(void) { require(("this will fail", false)); }
+
+void something_that_wont_fail(void) { require(("this will pass", 1 == 1)); }
+
+int main(void)
+{
+    assert(("this will pass", 1 == 1));
+    TRY {
+        something_that_wont_fail();
+        something_that_will_fail();
+    } CATCH(requirement_err) {
+        printf("%s", requirement_err.msg);
+    }
+    return EXIT_SUCCESS;
+}
